8720b4169e2aceaeeeb49f2cd7af8481d839e602
Erick Merchant <erickmerchant@gmail.com>
2018-08-26 22:46:49 -0700

    Switch to proxyquire for mocking

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
 const test = require('tape')
-const mockery = require('mockery')
-const mockerySettings = {
-  useCleanCache: true,
-  warnOnReplace: false,
-  warnOnUnregistered: false
-}
+const proxyquire = require('proxyquire').noPreserveCache()
 
 test('false - returned value', function (t) {
   t.plan(2)
@@ -19,77 +14,57 @@ test('false - returned value', function (t) {
 })
 
 test('true - watching', function (t) {
-  mockery.enable(mockerySettings)
-
   t.plan(4)
 
-  mockery.registerMock('debounce-fn', function (fn, options) {
-    t.deepEqual(options, {wait: 150})
+  proxyquire('./index', {
+    'debounce-fn': function (fn, options) {
+      t.deepEqual(options, {wait: 150})
 
-    return fn
-  })
+      return fn
+    },
+    'recursive-watch': function (directory, fn) {
+      t.equal(directory, './foo')
 
-  mockery.registerMock('recursive-watch', function (directory, fn) {
-    t.equal(directory, './foo')
-
-    fn('a-file')
-  })
-
-  require('./index')(true, './foo', function (files) {
+      fn('a-file')
+    }
+  })(true, './foo', function (files) {
     t.ok(1)
   })
-
-  mockery.disable()
-
-  mockery.deregisterAll()
 })
 
 test('true - debounced', function (t) {
-  mockery.enable(mockerySettings)
-
   t.plan(2)
 
-  mockery.registerMock('recursive-watch', function (directory, fn) {
-    t.equal(directory, './foo')
+  proxyquire('./index', {
+    'recursive-watch': function (directory, fn) {
+      t.equal(directory, './foo')
 
-    fn('a-file')
+      fn('a-file')
 
-    fn('b-file')
-  })
-
-  require('./index')(true, './foo', function (files) {
+      fn('b-file')
+    }
+  })(true, './foo', function (files) {
     if (files) {
       t.deepEqual(files, ['a-file', 'b-file'])
     }
   })
-
-  mockery.disable()
-
-  mockery.deregisterAll()
 })
 
 test('true - multiple', function (t) {
-  mockery.enable(mockerySettings)
-
   t.plan(2)
 
   let directories = []
 
-  mockery.registerMock('debounce-fn', function (fn, options) {
-    return fn
-  })
-
-  mockery.registerMock('recursive-watch', function (directory, fn) {
-    directories.push(directory)
-  })
-
-  require('./index')(true, ['./foo', './bar'], function (files) {
+  proxyquire('./index', {
+    'debounce-fn': function (fn, options) {
+      return fn
+    },
+    'recursive-watch': function (directory, fn) {
+      directories.push(directory)
+    }
+  })(true, ['./foo', './bar'], function (files) {
     t.ok(1)
 
     t.deepEqual(directories, ['./foo', './bar'])
   })
-
-  mockery.disable()
-
-  mockery.deregisterAll()
 })
